a3218ca0a5713f2c4577c4c7678433a730b9018d
deveshdatwani <datwanidevesh@gmail.com>
2023-03-09 20:45:29 -0500

    touching model and utils modules

---
+++ yolov1/model.py
@@ -0,0 +1,25 @@
+""" 
+
+Building the model architecture here
+Author: Devesh Datwani
+Date: March 09, 2023
+MIT License
+
+"""
+
+import torch
+from torch import nn
+
+
+
+class Model(nn.module):
+
+
+    def __init__(self):
+        return
+    
+
+    def forward(self, x):
+        return y
+
+
---
+++ yolov1/utils.py
@@ -0,0 +1,5 @@
+""" 
+
+Module for implementing utils
+
+"""
