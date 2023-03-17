676a33157d7cef48fe8dae8b1202bf84d8c1db05
deveshdatwani <datwanidevesh@gmail.com>
2023-03-17 19:19:59 -0400

    first version of the visualizer

--- README.md
+++ README.md
@@ -2,6 +2,16 @@
 
 This rep is to solve multi object detection with deep neural networks.
 
-I will start off by implementing the YOLO network proposed by Joseph Redmon. From here on, I will increase the complexity of architecture by implementing sub systems such as spatial transformer network, attention transformers, feature pyramid network, so on and so forth. 
+I will start by implementing the first version of the YOLO network. The idea is to subsequently increase the complexity of architecture by implementing sub systems such as spatial transformer network, attention transformers, feature pyramid network, so on and so forth to see how each hyperparameter affects the accuracies. 
 
 ### Dataset
+
+The dataset is a subset of the VOC dataset which contains 5000 images annotated with bounding boxes of objects present in the images.
+
+Let's visualze 25 images from the dataset. For this, I created a class for visualizing the dataset. It is fairly flexibe and modular. It randomly samples 25 images from the dataset directory and plots it on a grid with matplotlib. So far, it does it's job reasonably. 
+
+Let's run it
+
+
+
+
---
+++ yolov1/visualizer.py
@@ -0,0 +1,50 @@
+from matplotlib import pyplot as plt
+from math import sqrt
+import os
+from random import randint
+import cv2
+
+
+class dataVisualizer():
+    """
+    author: devesh datwani 
+    
+    This class creates a visualizer for an image dataset through matplotlib
+    The constructor requires a dataset address when initializing an object
+    The visualize member function should be used to visualize random images in the dataset
+    To change the grid size, the member function set_grid_size should be used
+    
+    """
+    
+    def __init__(self, cols=5, rows=2, data_directory=None, number_of_images=25) -> None:
+        self._cols = cols
+        self._rows = rows
+        self._data_directory = data_directory
+        self._figsize = (10,10)
+        self.number_of_images = number_of_images
+
+    def visualize(self) -> None:
+        image_cell = int(sqrt(self.number_of_images))
+        image_list = os.listdir(self._data_directory)
+        
+        for i in range(self.number_of_images):
+            random_id = randint(0, len(image_list)-1)
+            image_name = image_list[random_id]
+            image = cv2.imread(self._data_directory + "/" + image_name)
+            plt.subplot(image_cell, image_cell, i+1)
+            plt.imshow(image)
+        plt.show()
+        
+    
+    def set_grid_size(self, cols: int, rows: int) -> None:
+        self.cols = cols
+        self.rows = rows
+
+    def set_figsize(self, figsize: tuple) -> None:
+        self._figsize = figsize
+
+
+if __name__ == "__main__":
+    dataset_address = "/home/deveshdatwani/Datasets/voc/images"
+    visualizer = dataVisualizer(data_directory=dataset_address) 
+    visualizer.visualize()
