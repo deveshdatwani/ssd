27c0b367f50966f86f4a898144564252f4f83329
deveshdatwani <datwanidevesh@gmail.com>
2023-03-17 23:13:22 -0400

    minor changes

--- yolov1/visualizer.py
+++ yolov1/visualizer.py
@@ -13,8 +13,8 @@ class dataVisualizer():
     The constructor requires a dataset address when initializing an object
     The visualize member function should be used to visualize random images in the dataset
     To change the grid size, the member function set_grid_size should be used
-    
     """
+    
     
     def __init__(self, cols=5, rows=2, data_directory=None, number_of_images=25) -> None:
         self._cols = cols
@@ -23,6 +23,7 @@ class dataVisualizer():
         self._figsize = (10,10)
         self.number_of_images = number_of_images
 
+
     def visualize(self) -> None:
         image_cell = int(sqrt(self.number_of_images))
         image_list = os.listdir(self._data_directory)
@@ -30,9 +31,10 @@ class dataVisualizer():
         for i in range(self.number_of_images):
             random_id = randint(0, len(image_list)-1)
             image_name = image_list[random_id]
-            image = cv2.imread(self._data_directory + "/" + image_name)
+            image = cv2.imread(os.path.join(self._data_directory, image_name))
             plt.subplot(image_cell, image_cell, i+1)
             plt.imshow(image)
+        
         plt.show()
         
     
@@ -40,8 +42,10 @@ class dataVisualizer():
         self.cols = cols
         self.rows = rows
 
+
     def set_figsize(self, figsize: tuple) -> None:
         self._figsize = figsize
+
 
 
 if __name__ == "__main__":
