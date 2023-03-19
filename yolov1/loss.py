6310cd95de36d3955677009a74dce0d1a79ad625
deveshdatwani <datwanidevesh@gmail.com>
2023-03-19 13:58:29 -0400

    loss criterion unclear

--- yolov1/loss.py
+++ yolov1/loss.py
@@ -12,18 +12,23 @@ class Criterion(nn.Module):
     The forward function implments the loss function of YOLO model as described in the paper
     """
 
-    def __init__(self, lambda_coord=5, lambda__nooj=0.5, S=7, B=2, C=20):
+    def __init__(self, lambda_coord=5, lambda__nooj=0.5, S=7, B=2, C=20, im_width=448, im_height=448):
         self.lambda_coord = lambda_coord    
         self.lambda_noobj = lambda__nooj
         self.S = S
         self.B = B
         self.C = C
+        self.im_wdith = im_width
+        self.im_height = im_height
 
     def __forward__(self, x_output, target):
         loss = x_output - target
 
-
         return loss
+    
+    def IOU(self, x_output, target):
+        
+        return None
 
 
 if __name__ == "__main__":
