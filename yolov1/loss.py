1c8059f887f3aa30072f4d951715d5eea5ef3d9d
deveshdatwani <datwanidevesh@gmail.com>
2023-05-24 19:36:09 -0400

    yolo loss

--- yolov1/loss.py
+++ yolov1/loss.py
@@ -8,31 +8,32 @@ import torch
 
 
 class Criterion(nn.Module):
-    """
-    This class inherits from the Module class and calculates loss value for a given prediction
-    The forward function implments the loss function of YOLO model as described in the paper
-    """
-
-    def __init__(self, lambda_coord=5, lambda__nooj=0.5, S=7, B=2, C=20, im_width=448, im_height=448):
+    '''
+    Implments loss function of YOLO V1 model as per the paper
+    https://arxiv.org/pdf/1506.02640.pdf 
+    '''
+    def __init__(
+            self, 
+            lambda_coord=5, 
+            lambda__nooj=0.5, 
+            S=7, B=2, C=20, 
+            im_width=448, 
+            im_height=448
+        ):
         self.lambda_coord = lambda_coord    
         self.lambda_noobj = lambda__nooj
         self.S = S
         self.B = B
         self.C = C
-        self.im_wdith = im_width
-        self.im_height = im_height
         self.mse_loss = MSELoss(reduction="sum")
+
 
     def forward(self, predictions, target):
         
-        # This is not going to be easy  
-
         return None
 
 
 if __name__ == "__main__":
-
-
     out = torch.rand((1,7,7,30))
     target = torch.rand((1, 7, 7, 30))
     criterion = Criterion()
