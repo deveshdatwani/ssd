450345676d00dab0d53c9186bb4d628dd11eafd1
deveshdatwani <datwanidevesh@gmail.com>
2023-05-29 18:40:17 -0400

    aping aladin

--- yolov1/model.py
+++ yolov1/model.py
@@ -8,94 +8,23 @@ from torchvision.transforms import Resize
 from matplotlib import pyplot as plt
 
 
+class CNNBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, **kwargs):
+        super(CNNBlock, self).__init__()
+        self.conv = 
+
+
 
 class YOLO(nn.Module):
     """ 
-    author: devesh datwani
-
-    This class builds a pytorch model based on the first version of YOLO network
-    This class will be pretty inflexible and pretty straightforward
-    Thereforce, no choice of backbone, 
-
+    YOLO V1 network
     """
 
     def __init__(self):
         super(YOLO, self).__init__()
+    
 
-        self.c_layer1 = Conv2d(3, 64, 7, 2, padding=3)
-        self.c_layer2 = Conv2d(64, 192, 3, 1, padding='same')
-        self.c_layer3 = Conv2d(192, 128, 1, 1, padding='same')
-        self.c_layer4 = Conv2d(128, 256, 3, 1, padding='same')
-        self.c_layer5 = Conv2d(256, 256, 1, 1, padding='same')
-        self.c_layer6 = Conv2d(256, 512, 3, 1, padding='same')
-        self.c_layer7 = Conv2d(512, 256, 1, 1, padding='same')
-        self.c_layer8 = Conv2d(256, 512, 3, 1, padding='same')
-        self.c_layer9 = Conv2d(512, 512, 1, 1, padding='same')
-        self.c_layer10 = Conv2d(512, 1024, 3, 1, padding='same')
-        self.c_layer11 = Conv2d(1024, 512, 1, 1, padding='same')
-        self.c_layer12 = Conv2d(512, 1024, 3, 1, padding='same')               
-        self.c_layer13 = Conv2d(1024, 1024, 3, 2, padding=1)
-        self.c_layer14 = Conv2d(1024, 1024, 3, 1, padding='same')
+    def forward(self, x):
         
-        self.linear1 = Linear(7*7*1024, 1024)
-        self.linear2 = Linear(1024, 7*7*30)
-
-        self.mp_layer1 = MaxPool2d(2, stride=2)
-
-        self.leaky_relu = LeakyReLU(0.1)
-        
-        self.flatten = Flatten(0, -1)
-
-        # self.batchnorm = BatchNorm2d()
-        
-
-    def forward(self, x) :
-        x = self.c_layer1(x)
-        x = self.leaky_relu(x)
-        x = self.mp_layer1(x)
-        x = self.c_layer2(x)
-        x = self.leaky_relu(x)
-        x = self.mp_layer1(x)
-        x = self.c_layer3(x)
-        x = self.leaky_relu(x)
-        x = self.c_layer4(x)
-        x = self.leaky_relu(x)
-        x = self.c_layer5(x)
-        x = self.leaky_relu(x)
-        x = self.c_layer6(x)
-        x = self.leaky_relu(x)
-        x = self.mp_layer1(x)
-        
-        # x4
-        for i in range(4):
-            x = self.c_layer7(x)
-            x = self.leaky_relu(x)
-            x = self.c_layer8(x)
-            x = self.leaky_relu(x)
-
-        x = self.c_layer9(x)
-        x = self.leaky_relu(x)
-        x = self.c_layer10(x)
-        x = self.leaky_relu(x)
-        x = self.mp_layer1(x)
-
-        # x2
-        for i in range(2):
-            x = self.c_layer11(x)
-            x = self.leaky_relu(x)
-            x = self.c_layer12(x)
-            x = self.leaky_relu(x)
-
-        x = self.c_layer13(x)
-        x = self.leaky_relu(x)
-        x = self.c_layer14(x)
-        x = self.leaky_relu(x)
-        x = self.c_layer14(x)
-        x = self.leaky_relu(x)
-        x = self.flatten(x)
-        x = self.linear1(x)
-        x = self.leaky_relu(x) 
-        x = self.linear2(x)
-
         return x
 
