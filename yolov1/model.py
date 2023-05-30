0122c181a9a1bada6c9afff01e24ea0359e29e7e
deveshdatwani <datwanidevesh@gmail.com>
2023-05-30 19:08:01 -0400

    model and config ready

--- yolov1/model.py
+++ yolov1/model.py
@@ -1,11 +1,6 @@
-
 import torch
 from torch import nn
-from torch.functional import F
-from torch.nn import Conv2d, MaxPool2d, LeakyReLU, BatchNorm2d, Linear, Flatten
-from torchvision.io import read_image
-from torchvision.transforms import Resize
-from matplotlib import pyplot as plt
+from config import architecture_config
 
 
 class CNNBlock(nn.Module):
@@ -14,6 +9,7 @@ class CNNBlock(nn.Module):
         self.conv = nn.Conv2d(in_channels, out_channels, bias=False, **kwargs)
         self.batchnorm = nn.BatchNorm2d(out_channels)
         self.leakyrelu = nn.LeakyReLU(0.1)
+
 
     def forward(self, x):
         x = self.conv(x)
@@ -28,10 +24,69 @@ class YOLO(nn.Module):
     YOLO V1 network
     """
 
-    def __init__(self):
+    def __init__(self, in_channels=3, architectire_config=None, **kwargs):
         super(YOLO, self).__init__()
+        self.architecture = architecture_config
+        self.in_channels = in_channels
+        self.split_size = 7 
+        self.num_boxes = 2 
+        self.num_classes = 20
+        self._darknet = self._create_conv_layers(self.architecture)
+        self._fcs = self._create_fcs(self.split_size, self.num_boxes, self.num_classes)
     
 
-    def forward(self, x):
+    def _create_conv_layers(self, architecture):
+        layers = []
+        in_channels = self.in_channels
+
+        for i, x in enumerate(architecture):
+
+            if type(x) == tuple:    
+                layers += [
+                    CNNBlock(
+                        in_channels, x[1], kernel_size=x[0], stride=x[2], padding=x[3]
+                    )
+                ]
+                in_channels = x[1]
+
+            elif type(x) == str: layers += [nn.MaxPool2d(kernel_size=(2,2), stride=(2,2))]
+            elif type(x) == list: 
+                conv1 = x[0]
+                conv2 = x[1]
+                num_repeats = x[2]
+                
+                for _ in range(num_repeats):
+                    layers += [
+                        CNNBlock(
+                            in_channels, conv1[1], kernel_size=conv1[0], stride=conv1[2], padding=conv1[3]
+                        )
+                    ]
+                    layers += [
+                        CNNBlock(
+                            conv1[1], conv2[1], kernel_size=conv2[0], stride=conv2[2], padding=conv2[3]
+                        )
+                    ]
+                    in_channels = conv2[1]
         
+        return nn.Sequential(*layers) 
+    
+
+    def _create_fcs(self, split_size, num_boxes, num_classes):
+        S, B, C = split_size, num_boxes, num_classes
+        fcs = nn.Sequential(
+                            nn.Flatten(),
+                            nn.Linear(1024 * S * S, 496),
+                            nn.Dropout(0.0),
+                            nn.LeakyReLU(0.1),
+                            nn.Linear(496, S * S * (C + B * 5))
+            )
+        
+        return fcs
+
+
+    def forward(self, x):
+        x = self._darknet(x)
+        x = torch.flatten(x, start_dim=1)
+        x = self._fcs(x)
+
         return x
